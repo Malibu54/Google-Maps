3e9d642bac0bf7b03af871ebe516225f4f329f95
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-06-24 13:57:13 -0300

    add readme description

---
+++ README.md
@@ -0,0 +1,52 @@
+# Calculator
+
+This is a simple Google Maps web application built with HTML, CSS, and JavaScript. It allows users to add a marker to the map, which displays an info window when clicked. This feature provides additional information about a specific location on the map. <br>
+![Calculator Screenshot](/img/Maps.png)
+
+## Features
+
+- When the map loads, a marker is placed on the map at a predefined location, in this case, "Argentina, Zanjan.Buenos Aires
+The marker is represented by an icon and appears as a visual indicator on the map.
+- When a user clicks on the marker, an info window pops up, displaying the location name, in this case, "Argentina, Buenos Aires."
+- The info window provides additional details or context about the location.
+- The info window is styled and positioned to be visually appealing and user-friendly.
+- The info window remains open until the user clicks outside of it or clicks on the marker again.
+
+## Demo
+
+You can try out Google Maps by visiting the [live demo](https://malibucalculator.netlify.app/).
+
+## Getting Started
+
+To run the calculator locally, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/Malibu54/Calculator.git`
+2. Open the project folder.
+3. Open the `index.html` file in a web browser.
+
+## Usage
+
+// Create a marker for a specific location
+const marker = new google.maps.Marker({
+    position: new google.maps.LatLng(36.6769, 48.4963),
+    map: map,
+    title: "Iran, Zanjan"
+});
+
+// Add click event listener to the marker
+marker.addListener("click", function() {
+    infoWindow.setContent(marker.title);
+    infoWindow.open(map, marker);
+});
+
+
+
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
--- main.js
+++ main.js
@@ -1,34 +1,40 @@
 //Set map
 
 function initialize() {
-    let mapOptions = {
-        //Zoom of map of start 
-        zoom: 10 %;
-        //Initial center cordinates on start(NewYork)
-        center: new google.maps.LatLng(40.7128, -74.0060),
-        //Type of map (ROADMAP, SATELLITE, hybrid, terrain)
-        mapTypeID: google.maps.MapTypeId.ROADMAP,
-        //Minium zoom of map
-        minZoom: 2
-    };
+  let mapOptions = {
+    //Zoom of map of start
+    zoom: 10,
+    //Initial center cordinates on start(NewYork)
+    center: new google.maps.LatLng(40.7128, -74.006),
+    //Type of map (ROADMAP, SATELLITE, hybrid, terrain)
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //Minium zoom of map
+    minZoom: 2,
+  };
 
-    //Create a new map instance using provided options
-    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //Create a new map instance using provided options
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    //Create an info windows to display location info
-    let infoWindows = new google.maps.InfoWindow();
-    //Creare a marker for example: Iran, Zanjan
-    let = new google.maps.Marker({
-        //Cordinates for Iran, Zanjan
-        position: new google.maps.LatLng(36.6769, 48.4963),
-        //Attach the marer
-        map: map,
-        //Tooltip on hover
-        title: "Iran, Zanjan"
-    });
+  //Create an info windows to display location info
+  let infoWindows = new google.maps.InfoWindow();
+  //Creare a marker for example: Argentina, Buenos Aires
+  let marker = new google.maps.Marker({
+    position: new google.maps.LatLng(34.61315, 58.37723),
+    map: map,
+    title: "Argentina, Buenos Aires",
+  });
 
+  //Add click event listener for marker
+  marker.addDomListener("click", function () {
+    infoWindow.setContent(marker.title);
+    infoWindows.open(map, marker);
+  });
 
+  //Adjust map center when the windows is resized
+  google.maps.event.addDomListener(window, "resize", function () {
+    map.setCenter(mapOptions.center);
+  });
 }
 
 //Initialize the map when windows loading finished
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "load", initialize);
